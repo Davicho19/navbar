ead42c806e6fa95a34134e087e1c7fef5ab9139a
Ripple Shah <rippleshah@msn.com>
2019-01-31 06:52:30 -0500

    added if statement for data

--- menubar/src/app/cancellation/cancellation.component.ts
+++ menubar/src/app/cancellation/cancellation.component.ts
@@ -13,64 +13,74 @@ export class CancellationComponent implements OnInit {
   @Input('zipCode') zipCode: string;
   @Input('lastName') lastName: string;
   customerNames: Array<CustomerNameCancellation>;
-  
+
   private api: GridApi;
   columnApi: ColumnApi;
   columnDefs: ColDef[];
   loadCancellation: boolean;
   params: any;
 
-  constructor (private customerNameService: CustomerNameService) {
-   this.columnDefs = this.createColumnDefs(); 
+  constructor(private customerNameService: CustomerNameService) {
+    this.columnDefs = this.createColumnDefs();
   }
 
   ngOnInit() {
     this.customerNameService
-    .getCustomerNames(this.zipCode, this.lastName)
-    .subscribe(data => {this.customerNames = data;})
+      .getCustomerNames(this.zipCode, this.lastName)
+      .subscribe(data => { this.customerNames = data; })
     this.loadCancellation = true;
   }
 
-  onGridReady(params) : void {
+  onGridReady(params): void {
     this.api = params.api;
     this.columnApi = params.columnApi;
     this.api.sizeColumnsToFit();
+    this.customerNameService
+      .getCustomerNames(this.zipCode, this.lastName)
+      .subscribe(data => {
+
+        if (data) {
+          this.customerNames = data;
+          console.log(this.customerNames);
+        }
+      });
 
     if (this.api) {
       console.log("inside if this.api");
       this.api.forEachNode((node) => {
-          console.log("node" + node);
+        console.log("node" + node);
       });
     }
   }
 
-    // this.api.forEachNode((node) => {
-      // console.log("Inside gridReady");
-      // this.customerNames.map((customer) => {
-      //   console.log(customer);
-      //   if (customer.IsCancel) {
-      //     node.setSelected(true);
-      //   }
-      // });
-    // });
+  // this.api.forEachNode((node) => {
+  // console.log("Inside gridReady");
+  // this.customerNames.map((customer) => {
+  //   console.log(customer);
+  //   if (customer.IsCancel) {
+  //     node.setSelected(true);
+  //   }
+  // });
+  // });
 
-  
+
 
   private createColumnDefs() {
     return [
-      {headerName: 'Customer Number', field: 'CustomerNumber', sortable: true, filter: true},
-      {headerName: 'First Name', field: 'FirstName', sortable: true, filter: true},
-      {headerName: 'Middle Name', field: 'MiddleName', sortable: true, filter: true},
-      {headerName: 'Last Name', field: 'LastName', sortable: true, filter: true},
-      {headerName: 'Address', field: 'Address1', sortable: true, filter: true},
-      {headerName: 'City', field: 'City', sortable: true, filter: true},
-      {headerName: 'State', field: 'StateCd', sortable: true, filter: true},
-      {headerName: 'Zip Code', field: 'ZipCode', sortable: true, filter: true},
-      {headerName: 'Magazine Code', field: 'MagazineCd', sortable: true, filter: true},
-      {headerName: 'IsCancel', 
+      { headerName: 'Customer Number', field: 'CustomerNumber', sortable: true, filter: true },
+      { headerName: 'First Name', field: 'FirstName', sortable: true, filter: true },
+      { headerName: 'Middle Name', field: 'MiddleName', sortable: true, filter: true },
+      { headerName: 'Last Name', field: 'LastName', sortable: true, filter: true },
+      { headerName: 'Address', field: 'Address1', sortable: true, filter: true },
+      { headerName: 'City', field: 'City', sortable: true, filter: true },
+      { headerName: 'State', field: 'StateCd', sortable: true, filter: true },
+      { headerName: 'Zip Code', field: 'ZipCode', sortable: true, filter: true },
+      { headerName: 'Magazine Code', field: 'MagazineCd', sortable: true, filter: true },
+      {
+        headerName: 'IsCancel',
         // field:'IsCancel',
-        editable: true, 
-        checkboxSelection:true 
+        editable: true,
+        checkboxSelection: true
       },
       // {header: 'Cancel', 
       //   field: 'IsCancel', 
@@ -78,7 +88,7 @@ export class CancellationComponent implements OnInit {
       //     return `<input type='checkbox' ${params.value ? 'checked': ''} "/>`
       //   }  
       // },
-      {headerName: 'Cancellation Date', field: 'CancellationDate', sortable: true, filter: true}
+      { headerName: 'Cancellation Date', field: 'CancellationDate', sortable: true, filter: true }
     ]
   }
 
@@ -100,7 +110,7 @@ export class CancellationComponent implements OnInit {
 
   public checkedVal() {
     console.log(this.params.node.data);
-    console.log(this.params.value); 
+    console.log(this.params.value);
   }
 
 }
